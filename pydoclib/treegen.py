957db8083a8fc81d2579c763b8c741e2ddb5cacd
Dave Jackson <dave@jacksonofalltrades.net>
2015-07-11 14:18:59 -0700

    Updates-working now!

--- pydoclib/treegen.py
+++ pydoclib/treegen.py
@@ -5,9 +5,36 @@ import sys
 import os
 from os.path import join, getsize
 import re
+import json
 
 def is_pkg(path_parts, files):
     return "__init__.py" in files
+
+def count_class_refs(class_name, all_files):
+    """
+    Patterns:
+    =\s+<class_name>\s*\(
+    <class_name>\.
+    """
+    refs = 0
+    pats = [
+        '=\s+%s\s*\(' % class_name,
+        '\s+%s\.' % class_name
+    ]
+    print "Searching for refs to class [%s]..." % class_name
+    for fpath in all_files:
+        with open(fpath, "r") as f:
+            for line in f:
+                for p in pats:
+                    matches = re.findall(p, line)
+                    refs += len(matches)
+    print "\tFound %s!" % refs
+    
+    if refs > 0:
+        return refs
+    else:
+        return 1
+
 
 def add_to_tree(pkg_tree, pkg_node_path):
     curr_node = pkg_tree
@@ -26,14 +53,58 @@ def add_mod_classes(coderoot, modules):
         # m['mod_contents'] = path_str
         m['classes'] = re.findall("class ([a-zA-Z0-9_]+)\(", open(mod_file_path, 'r').read())
 
+def build_tree_map(all_files, pkg_tree):
+    treemap_data = []
+    """
+    {
+        "name": "sample package",
+        "tree": {
+            "name": "flare",
+            "children": [
+            ]
+        }
+    }
+    """
+    
+    if type(pkg_tree) == dict:
+        for key, val in pkg_tree.items():
+            if key == '__modules':
+                for mod, classlist in map(lambda x: (x['mod'], x['classes']), filter(lambda y: y['classes'], val)):
+                    node = {
+                        'name': mod,
+                        'children': build_tree_map(all_files, classlist)
+                    }
+                    treemap_data.append(node)
+            else:
+                node = {
+                    'name': key,
+                    'children': build_tree_map(all_files, val)
+                }
+                treemap_data.append(node)
+    elif len(pkg_tree) and type(pkg_tree) == list:
+        for c in pkg_tree:
+            node = {
+                'name': c,
+                'value': count_class_refs(c, all_files)
+            }
+            treemap_data.append(node)
+
+    return treemap_data
+
+
 def make_pkg_tree(coderoot):
     if os.path.exists(coderoot):
         # Add to the system path so we can import modules
         sys.path.append(coderoot)
         
+        treename = os.path.basename(coderoot)
+        
         pkg_tree = {}
         
+        all_files = []
+        
         for root, dirs, files in os.walk(coderoot):
+            all_files.extend(map(lambda x: os.path.join(root, x), files))
             subdir_parts = filter(lambda x: x, root[len(coderoot):].split("/"))
             if subdir_parts:
                 print "pkg: %s" % subdir_parts
@@ -48,6 +119,20 @@ def make_pkg_tree(coderoot):
                 #if 'CVS' in dirs:
                 #    dirs.remove('CVS')  # don't visit CVS directories
                 """
-        return pkg_tree
+        
+        # Convert to format needed for d3 tree map
+        treemap_data = {
+            'name': treename,
+            'children': build_tree_map(all_files, pkg_tree)
+        }
+        
+        meta = {
+            'tree': treemap_data,
+            'name': treename
+        }
+        
+        with open(os.path.join(coderoot, "package.json"), 'w') as f:
+            json.dump(meta, f, indent=4, sort_keys=True)
+
     else:
         raise Exception("Code root folder [%s] not found!" % coderoot)
